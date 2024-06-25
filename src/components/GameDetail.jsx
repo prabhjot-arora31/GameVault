13afcd1ed9fda37fff5d212176898b0b31db8490
Prabhjot Arora <83449967+prabhjot-arora31@users.noreply.github.com>
2024-06-25 16:29:56 +0530

    Update GameDetail.jsx

--- src/components/GameDetail.jsx
+++ src/components/GameDetail.jsx
@@ -24,7 +24,7 @@ const GameDetail = ({ individualData, setIsDetailedPage, darkMode }) => {
         </div>
       <h2
         style={{
-          marginTop:'45px',
+          marginTop:'60px',
           marginBottom: "4px",
           fontSize: "35px",
           color: darkMode ? "white" : "black",
